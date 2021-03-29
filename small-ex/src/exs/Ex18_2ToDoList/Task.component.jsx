51372f4c5bebb112f547cb3fa356acbe4e295ec4
miraleJ <miralek@gmail.com>
2021-03-30 01:41:25 +0300

    ex18.2 fix

--- small-ex/src/exs/Ex18_2ToDoList/Task.component.jsx
+++ small-ex/src/exs/Ex18_2ToDoList/Task.component.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './style.css'
 
 export default function Task(props) {
+    const [cName, setClassName] = useState('completed');
+
+    useEffect(() => {
+        props.completed ? setClassName('completed') : setClassName('notcompleted')
+      }, []);
+
     const showCompleted = () => {
         return props.completed ? <>&#x02713;
         </> : <>&#x02717;</>;
     }
 
+    const handle = (event) => {
+        cName === 'notcompleted' ? setClassName('completed') : setClassName('notcompleted')
+        props.onClick(event);
+    }
+
     return (
         <div>
-            <p onClick={props.onClick}>
+            <p onClick={handle} className={cName}>
                 <span>{showCompleted()}</span>
                 {props.taskText}
             </p>
